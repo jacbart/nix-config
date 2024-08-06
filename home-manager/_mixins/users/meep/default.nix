c2ef55f4b5bdfaa9a996e163832d8a81aa80047b
Jack Bartlett <jacbart@gmail.com>
2024-08-05 19:11:16 -0700

    ohmyzsh in zplug

--- home-manager/_mixins/users/meep/default.nix
+++ home-manager/_mixins/users/meep/default.nix
@@ -90,10 +90,10 @@
       zplug = {
         enable = true;
         plugins = [
-          { name = "ohmyzsh/ohmyzsh/plugins/fzf"; }
-          { name = "ohmyzsh/ohmyzsh/plugins/git"; }
-          { name = "ohmyzsh/ohmyzsh/plugins/tmux"; }
-          { name = "ohmyzsh/ohmyzsh/plugins/z"; }
+          { name = "plugins/fzf"; tags = "from:oh-my-zsh"; }
+          { name = "plugins/git"; tags = "from:oh-my-zsh"; }
+          { name = "plugins/tmux"; tags = "from:oh-my-zsh"; }
+          { name = "plugins/z"; tags = "from:oh-my-zsh"; }
           { name = "zsh-users/zsh-autosuggestions"; }
           { name = "zsh-users/zsh-syntax-highlighting"; }
           { name = "zsh-users/zsh-completions"; }
