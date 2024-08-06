70f991897548af54ed62b0c973f864db0e57bbb9
Jack Bartlett <jacbart@gmail.com>
2024-08-06 13:50:20 -0700

    rockpro64 syntax fixes

--- nixos/_mixins/hardware/rockpro64.nix
+++ nixos/_mixins/hardware/rockpro64.nix
@@ -7,7 +7,7 @@
   # This list of modules is not entirely minified, but represents
   # a set of modules that is required for the display to work in stage-1.
   # Further minification can be done, but requires trial-and-error mainly.
-  boot.initrd.availableKernelModules = [ "usb_storage" "usbhid" ]
+  boot.initrd.availableKernelModules = [ "usb_storage" "usbhid" ];
   boot.initrd.kernelModules = [
     # Rockchip modules
     "rockchip_rga"
@@ -19,5 +19,5 @@
   # The default powersave makes the wireless connection unusable.
   networking.networkmanager.wifi.powersave = lib.mkDefault false;
 
-  powerManager.cpuFreqGovernor = lib.mkDefault "ondemand";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
 }
