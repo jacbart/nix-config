b5e4b7c6251d2454dd8f273eb805090dcb7f352c
jacbart <jacbart@gmail.com>
2024-08-08 20:25:56 -0700

    using basic example from eww repo

--- home-manager/_mixins/desktop/eww.nix
+++ home-manager/_mixins/desktop/eww.nix
@@ -1,12 +1,7 @@
-{ config, pkgs, ... }: let
-    eww-config = builtins.fetchGit {
-        url = "https://github.com/saimoomedits/eww-widgets";
-        rev = "cfb2523a4e37ed2979e964998d9a4c37232b2975";
-    };
-in {
+{ config, pkgs, ... }: {
     home.packages = with pkgs; [
         eww
     ];
     
-    home.file."${config.xdg.configHome}/eww".source = "${eww-config}/eww/bar";
+    home.file."${config.xdg.configHome}/eww".source = ./eww;
 }
