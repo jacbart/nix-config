b539dda61d773e7d80f8406b7074e2d0acfcc66d
jacbart <jacbart@gmail.com>
2024-08-09 08:40:44 -0700

    setting the new eww bar instead of fetchGit

--- home-manager/_mixins/desktop/eww.nix
+++ home-manager/_mixins/desktop/eww.nix
@@ -1,13 +1,7 @@
-{ config, pkgs, ... }: let 
-    ewwconfig = builtins.fetchGit {
-        url = "https://github.com/owenrumney/eww-bar";
-        rev = "594295d5c0203c1c067a7c8004cf1d6fe835234b";
-    };
-in {
+{ config, pkgs, ... }: {
     home.packages = with pkgs; [
         eww
     ];
     
-    # home.file."${config.xdg.configHome}/eww".source = ./eww;
-    home.file."${config.xdg.configHome}/eww".source = ewwconfig;
+    home.file."${config.xdg.configHome}/eww".source = ./eww;
 }
