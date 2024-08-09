b498dbf03f6649d8912d5f3ce3eae6494b203c72
jacbart <jacbart@gmail.com>
2024-08-08 20:09:06 -0700

    fixing old code i left

--- home-manager/_mixins/desktop/eww.nix
+++ home-manager/_mixins/desktop/eww.nix
@@ -3,7 +3,7 @@
         eww-wayland
     ];
     theme = builtins.fetchGit {
-        url = "https://github.com/saimoomedits/eww-widgets/eww";
+        url = "https://github.com/saimoomedits/eww-widgets";
         rev = "cfb2523a4e37ed2979e964998d9a4c37232b2975";
     };
     home.file."${config.xdg.configHome}/eww".source = "${theme}/eww";
