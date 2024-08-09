69fb5223605e23c5ae5a134b34de5322832a51c1
jacbart <jacbart@gmail.com>
2024-08-08 17:24:28 -0700

    feat(nix): alias for nix building home and host

--- home-manager/_mixins/shell/zsh.nix
+++ home-manager/_mixins/shell/zsh.nix
@@ -18,6 +18,11 @@
         j = "z";
         gs = "git status";
         st = "stow -v -t $HOME";
+        nix-gc = "sudo nix-collect-garbage --delete-older-than 10d && nix-collect-garbage --delete-older-than 10d";
+        rebuild-all = "sudo nixos-rebuild switch --flake $HOME/workspace/personal/nix-config && home-manager switch -b backup --flake $HOME/workspace/personal/nix-config";
+        rebuild-home = "home-manager switch -b backup --flake $HOME/workspace/personal/nix-config";
+        rebuild-host = "sudo nixos-rebuild switch --flake $HOME/workspace/personal/nix-config";
+        rebuild-lock = "pushd $HOME/workspace/personal/nix-config && nix flake update && popd";
       };
       zplug = {
         enable = true;
