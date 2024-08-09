813922ca4235e3164eaa698918d27e5bef2c9c4b
jacbart <jacbart@gmail.com>
2024-08-08 20:12:44 -0700

    trying again

--- home-manager/_mixins/desktop/eww.nix
+++ home-manager/_mixins/desktop/eww.nix
@@ -1,10 +1,12 @@
-{ config, pkgs, ... }: rec {
-    home.packages = with pkgs; [
-        eww-wayland
-    ];
-    theme = builtins.fetchGit {
+{ config, pkgs, ... }: let
+    eww-config = builtins.fetchGit {
         url = "https://github.com/saimoomedits/eww-widgets";
         rev = "cfb2523a4e37ed2979e964998d9a4c37232b2975";
     };
-    home.file."${config.xdg.configHome}/eww".source = ${theme}/eww;
+in {
+    home.packages = with pkgs; [
+        eww-wayland
+    ];
+    
+    home.file."${config.xdg.configHome}/eww".source = "${eww-config}/eww";
 }
