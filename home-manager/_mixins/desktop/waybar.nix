8e3ef6fbc150ce9d754c097ba57178f63a64aa21
jacbart <jacbart@gmail.com>
2024-08-08 18:44:53 -0700

    fix(packages): switching away from programs, cant get the settings or style to work

--- home-manager/_mixins/desktop/waybar.nix
+++ home-manager/_mixins/desktop/waybar.nix
@@ -1,13 +1,10 @@
 { config, pkgs, ... }: {
-    # home = {
-    #     # waybar
-    #     file."${config.xdg.configHome}/waybar/config".text = builtins.readFile ./waybar/config.json;
-    #     file."${config.xdg.configHome}/waybar/style.css".text = builtins.readFile ./waybar/style.css;
-    # };
-    programs.waybar = {
-      enable = true;
-      package = pkgs.waybar;
-      settings = builtins.fromJSON (builtins.readFile ./waybar/config.json);
-      style = builtins.fromJSON (builtins.readFile ./waybar/style.css);
+    home = {
+        # waybar
+        file."${config.xdg.configHome}/waybar/config".text = builtins.readFile ./waybar/config.json;
+        file."${config.xdg.configHome}/waybar/style.css".text = builtins.readFile ./waybar/style.css;
     };
+    home.packages = with pkgs; [
+        waybar
+    ];
 }
