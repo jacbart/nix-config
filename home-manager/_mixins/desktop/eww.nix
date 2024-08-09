c68d19bb28383c92367e8af17f980126e75a2303
jacbart <jacbart@gmail.com>
2024-08-08 22:23:11 -0700

    trying a diff eww bar

--- home-manager/_mixins/desktop/eww.nix
+++ home-manager/_mixins/desktop/eww.nix
@@ -1,7 +1,13 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: let 
+    ewwconfig = builtins.fetchGit {
+        url = "https://github.com/owenrumney/eww-bar";
+        rev = "594295d5c0203c1c067a7c8004cf1d6fe835234b";
+    };
+in {
     home.packages = with pkgs; [
         eww
     ];
     
-    home.file."${config.xdg.configHome}/eww".source = ./eww;
+    # home.file."${config.xdg.configHome}/eww".source = ./eww;
+    home.file."${config.xdg.configHome}/eww".source = ewwconfig;
 }
