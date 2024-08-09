3a55d8fb9dc783303c12591e8eefc359e2d7df39
jacbart <jacbart@gmail.com>
2024-08-08 18:37:01 -0700

    feat(waybar): programs settings and style to see if that auto starts it

--- home-manager/_mixins/desktop/waybar.nix
+++ home-manager/_mixins/desktop/waybar.nix
@@ -1,11 +1,13 @@
 { config, pkgs, ... }: {
-    home = {
-        # waybar
-        file."${config.xdg.configHome}/waybar/config".text = builtins.readFile ./waybar/config;
-        file."${config.xdg.configHome}/waybar/style.css".text = builtins.readFile ./waybar/style.css;
-    };
+    # home = {
+    #     # waybar
+    #     file."${config.xdg.configHome}/waybar/config".text = builtins.readFile ./waybar/config.json;
+    #     file."${config.xdg.configHome}/waybar/style.css".text = builtins.readFile ./waybar/style.css;
+    # };
     programs.waybar = {
       enable = true;
       package = pkgs.waybar;
+      settings = builtins.readFile ./waybar/config.json;
+      style = builtins.readFile ./waybar/style.css;
     };
 }
