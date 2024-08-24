2f15346543a58cc781dac363ac69dbbdce348501
jacbart <jacbart@gmail.com>
2024-08-24 06:55:10 -0700

    fix(cosmic-greeter): displayManager NOT desktopManager

--- nixos/_mixins/desktop/cosmic.nix
+++ nixos/_mixins/desktop/cosmic.nix
@@ -1,37 +1,6 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    cosmic-applets
-    cosmic-applibrary
-    cosmic-bg
-    cosmic-comp
-    cosmic-design-demo
-    cosmic-edit
-    cosmic-emoji-picker
-    cosmic-files
-    cosmic-greeter
-    cosmic-icons
-    cosmic-launcher
-    cosmic-notifications
-    cosmic-osd
-    cosmic-panel
-    cosmic-protocols
-    cosmic-randr
-    cosmic-screenshot
-    cosmic-session
-    cosmic-settings-daemon
-    cosmic-settings
-    cosmic-store
-    cosmic-tasks
-    cosmic-term
-    cosmic-workspaces-epoch
-    libcosmicAppHook
-    pop-launcher
-    xdg-desktop-portal-cosmic
-  ];
-
-
-  services.desktopManager = {
-    cosmic.enable = true;
-    cosmic-greeter.enable = true;
+_: {
+  services = {
+    desktopManager.cosmic.enable = true;
+    displayManager.cosmic-greeter.enable = true;
   };
 }
