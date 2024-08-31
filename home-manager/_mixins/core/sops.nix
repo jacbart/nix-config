0001a0371844f1eeccdb93677c013f927b910f47
jacbart <jacbart@gmail.com>
2024-08-30 22:46:15 -0700

    changing back

--- home-manager/_mixins/core/sops.nix
+++ home-manager/_mixins/core/sops.nix
@@ -1,13 +1,9 @@
 { config, inputs, ... }: let
   secretsPath = builtins.toString inputs.mySecrets;
   homeDir = config.home.homeDirectory;
-  homeKeyPath = "${homeDir}/.config/sops/age/keys.txt";
 in {
   sops = {
-    age = {
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed22519_key" ];
-      keyFile = if builtins.pathExists homeKeyPath then homeKeyPath else "/var/lib/sops-nix/key.txt";
-    };
+    age.keyFile = "${homeDir}/.config/sops/age/keys.txt";
 
     defaultSopsFile = "${secretsPath}/secrets.yaml";
     validateSopsFiles = false;
