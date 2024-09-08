59c66d48e63a889260b9ce0e883a90bd918d2ead
jacbart <jacbart@gmail.com>
2024-09-08 09:06:23 -0700

    ssh config

--- home-manager/_mixins/shell/default.nix
+++ home-manager/_mixins/shell/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, username, ... }:
 # let
     # helper function for generating a shell (minimal, server, desktop)
     # mkDotenv = { type ? "minimal" }: {
@@ -11,4 +11,22 @@
         ./scripts # shellApplications
         ./zsh.nix # zsh config
     ];
+
+    home.file.".ssh/config".text = ''
+        Host boojum
+            HostName boojum.meep.sh
+            User meep
+            Port 22
+            IdentityFile ~/.ssh/id_ratatoskr
+
+        Host maple
+            HostName maple.meep.sh
+            User ratatoskr
+            Port 22
+            IdentityFile ~/.ssh/id_ratatoskr
+    '';
+
+    # systemd.tmpfiles.rules = [
+    #     "f ${config.home.homeDirectory} 0600 ${username}"
+    # ];
 }
