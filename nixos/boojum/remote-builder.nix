e045c720c8ee20c411e15d22216a9394110b5a75
jacbart <jacbart@gmail.com>
2024-09-09 10:35:19 -0700

    needed int

--- nixos/boojum/remote-builder.nix
+++ nixos/boojum/remote-builder.nix
@@ -13,8 +13,8 @@
     nrBuildUsers = 32;
     settings = {
       trusted-users = [ "remotebuild" ];
-      min-free = "10G";
-      max-free = "20G";
+      min-free = 10 * 1024 * 1024;
+      max-free = 50 * 1024 * 1024;
       max-jobs = "auto";
       cores = 0;
     };
