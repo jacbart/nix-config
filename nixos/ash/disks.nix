6ea27efe768ff8f6a9b4e7fb3fab5fd0b9c9f795
jacbart <jacbart@gmail.com>
2024-09-11 15:50:31 -0700

    fix missing empty disk

--- nixos/ash/disks.nix
+++ nixos/ash/disks.nix
@@ -1,1 +1,32 @@
-_:{}
+{ disks ? [ "/dev/mmcblk0" ], ... }:
+let
+  defaultExt4Opts = [ "defaults" ];
+in
+{
+  disko.devices = {
+    disk = {
+      mmcblk2 = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "gpt";
+          partitions = {
+            p1 = {
+              size = "30M";
+            };
+            root = {
+              name = "nixos";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+                mountOptions = defaultExt4Opts;
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
