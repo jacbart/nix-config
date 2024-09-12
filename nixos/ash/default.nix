d572d3f0f2f9b2178f18b6d5e380943f812de2a8
jacbart <jacbart@gmail.com>
2024-09-11 18:50:19 -0700

    ash config updates

--- nixos/ash/default.nix
+++ nixos/ash/default.nix
@@ -1,16 +1,10 @@
-{ lib, ... }: {
-  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-  boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
-
+{ ... }: {
   fileSystems = { 
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
       options = [
         "x-initrd"
-        "mount"
       ];
     };
     "/boot/firmware" = {
@@ -23,5 +17,5 @@
     };
   };
 
-  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+  nixpkgs.hostPlatform = "aarch64-linux";
 }
