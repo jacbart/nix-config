0c6e08b0bc5ab85061efb62748ccf555e2a38dfb
jacbart <jacbart@gmail.com>
2024-09-12 12:21:32 -0700

    trying to force the stateVersion

--- flake.nix
+++ flake.nix
@@ -60,8 +60,9 @@
     } @ inputs:
     let
       inherit (self) outputs;
+      inherit (nixpkgs) lib;
       # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-      stateVersion = "24.05";
+      stateVersion = lib.mkForce "24.05";
       libx = import ./lib { inherit inputs outputs stateVersion; };
     in
     {
