badccbd08c74afd62a4b82eb52b5623b8398627a
jacbart <jacbart@gmail.com>
2024-09-15 10:37:21 -0700

    iwd for ash

--- nixos/ash/default.nix
+++ nixos/ash/default.nix
@@ -27,5 +27,17 @@
       "127.0.0.1" = [ "ash" "ash.meep.sh"  ];
       "192.168.0.120" = [ "mesquite" "mesquite.meep.sh" ];
     };
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        Network = {
+          EnableIPv6 = true;
+          RoutePriorityOffset = 300;
+        };
+        Settings = {
+          AutoConnect = true;
+        };
+      };
+    };
   };
 }
