6a9e2c1f99e935cc90d9aef2f48f9817c1b49de3
jacbart <jacbart@gmail.com>
2024-09-18 18:40:11 -0700

    fix for wg

--- nixos/ash/wireguard.nix
+++ nixos/ash/wireguard.nix
@@ -5,15 +5,15 @@
   networking.wg-quick.interfaces = {
     wg0 = {
       address = [ "192.168.2.3/32" ];
-      dns = [ "192.168.2.1" ];
+      dns = [ "192.168.0.120" ];
       privateKeyFile = config.sops.secrets."wireguard/ash/private".path;
       
       peers = [
         {
           publicKey = "aGYKutq/jSiCOnjgJ0nZaM25qfMnEh3lHoyxwLGCVxo=";
           presharedKeyFile = config.sops.secrets."wireguard/ash/psKey".path;
-          allowedIPs = [ "0.0.0.0/0" "::/0" ];
-          endpoint = "wg.meep.sh:51999";
+          allowedIPs = [ "0.0.0.0/0" ];
+          endpoint = "192.168.0.120:51999";
           persistentKeepalive = 25;
         }
       ];
