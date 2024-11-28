90f835d8c80565276a783f16401e5e06391c15bc
jacbart <jacbart@gmail.com>
2024-11-27 21:32:19 -0700

    feat(headplane): expanded the config options for the headplane module but still need to test it

--- modules/services/web-apps/headplane.nix
+++ modules/services/web-apps/headplane.nix
@@ -16,17 +16,15 @@ in
       package = lib.mkPackageOption pkgs.local "headplane" { };
 
       user = lib.mkOption {
-        default = hs-cfg.user;
+        default = hs-cfg.user or "headscale";
         type = lib.types.str;
         description = ''
           User account under which headplane runs.
-
-
         '';
       };
 
       group = lib.mkOption {
-        default = hs-cfg.group;
+        default = hs-cfg.group or "headscale";
         type = lib.types.str;
         description = ''
           Group under which headplane runs
@@ -34,7 +32,7 @@ in
       };
 
       host = lib.mkOption {
-        default = hs-cfg.address;
+        default = hs-cfg.address or "0.0.0.0";
         type = lib.types.str;
         description = ''
           Listening address of headplane
@@ -50,16 +48,74 @@ in
       };
 
       headscale = lib.mkOption {
-        description = '''';
+        description = ''
+          Configuration related to integrating with the Headscale server.
+        '';
         type = lib.types.submodule {
           freeformType = settingsFormat.type;
 
           options = {
             config_file = lib.mkOption {
-              default = "TODO";
+              default = "/etc/headscale/config.yaml";
+              type = lib.types.path;
+              description = "The path to the Headscale config file.";
             };
           };
         };
+      };
+
+      cookieSecret = lib.mkOption {
+        type = lib.types.str;
+        description = ''
+          A secret used to sign cookies for Headplane.
+        '';
+      };
+
+      headscaleUrl = lib.mkOption {
+        type = lib.types.str;
+        description = ''
+          The public URL of your Headscale server.
+        '';
+      };
+
+      debug = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+        description = ''
+          Enable debug logging for Headplane.
+        '';
+      };
+
+      cookieSecure = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
+        description = ''
+          Ensure cookies are sent only over HTTPS.
+        '';
+      };
+    };
+  };
+
+  config = lib.mkIf config.services.headplane.enable {
+    systemd.services.headplane = {
+      description = "Headplane Service";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        User = config.services.headplane.user;
+        Group = config.services.headplane.group;
+        Environment = [
+          "COOKIE_SECRET=${config.services.headplane.cookieSecret}"
+          "HEADSCALE_URL=${config.services.headplane.headscaleUrl}"
+          "DEBUG=${toString config.services.headplane.debug}"
+          "HOST=${config.services.headplane.host}"
+          "PORT=${toString config.services.headplane.port}"
+          "CONFIG_FILE=${config.services.headplane.headscale.config_file}"
+          "COOKIE_SECURE=${toString config.services.headplane.cookieSecure}"
+        ];
+        ExecStart = "${config.services.headplane.package}/bin/headplane";
+        Restart = "always";
       };
     };
   };
