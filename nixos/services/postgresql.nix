c174b70d73194fadf4b9a6d3ea838a0fae71ffeb
jacbart <jacbart@gmail.com>
2024-12-28 15:16:00 -0700

    renamed postgres id maps

--- nixos/services/postgresql.nix
+++ nixos/services/postgresql.nix
@@ -18,11 +18,11 @@ _: {
     ];
     identMap = ''
       # ArbitraryMapName systemuser DBUser
-      superuser_map    root        postgres
-      superuser_map    postgres    postgres
-      superuser_map    zitadel     postgres
-      superuser_map    headscale   postgres
-      superuser_map    nextcloud   postgres
+      superuser_map_root  root        postgres
+      superuser_map_01    postgres    postgres
+      superuser_map_02    zitadel     postgres
+      superuser_map_03    headscale   postgres
+      superuser_map_04    nextcloud   postgres
       # Let other names login as themselves
       superuser_map    /^(.*)$     \1
     '';
