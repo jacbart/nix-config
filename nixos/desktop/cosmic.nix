bec7eab54810e178fe1d9f5cd001c31761322a48
jacbart <jacbart@gmail.com>
2025-01-01 21:09:54 -0700

    power daemon for boojum

--- nixos/desktop/cosmic.nix
+++ nixos/desktop/cosmic.nix
@@ -1,6 +1,10 @@
-_: {
+{ pkgs, ... }: {
   services = {
     desktopManager.cosmic.enable = true;
     displayManager.cosmic-greeter.enable = true;
   };
+
+  environment.systemPackages = [
+    pkgs.power-profiles-daemon
+  ];
 }
