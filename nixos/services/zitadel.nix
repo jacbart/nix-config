a9a8bb40ff7d7f6fea6a6d427c0e9011c9908fcd
jacbart <jacbart@gmail.com>
2025-02-05 22:49:24 -0700

    formatting changes

--- nixos/services/zitadel.nix
+++ nixos/services/zitadel.nix
@@ -93,10 +93,10 @@ in
         proxyWebsockets = true; # needed if you need to use WebSocket
         extraConfig =
           # required when the target is also TLS server with multiple hosts
-          "proxy_ssl_server_name on;" +
+          "proxy_ssl_server_name on;"
+          +
           # required when the server wants to use HTTP Authentication
-          "proxy_pass_header Authorization;"
-          ;
+          "proxy_pass_header Authorization;";
       };
     };
   };
