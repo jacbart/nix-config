248ec935e93290cfb4ea67a31f4f884bc3a4f5fb
jacbart <jacbart@gmail.com>
2025-02-18 08:33:05 -0700

    tmux config additional comments and cleaning up

--- home-manager/shell/tools/tmux.nix
+++ home-manager/shell/tools/tmux.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   home = {
-    packages = with pkgs; [ tmux ];
+    # Install tmux
+    packages = [ pkgs.tmux ];
 
     # Fetch the tmux plugin manager (tpm) and place it in the ~/.tmux/plugins/tpm directory
     file.".tmux/plugins/tpm".source = builtins.fetchGit {
@@ -8,6 +9,7 @@
       rev = "99469c4a9b1ccf77fade25842dc7bafbc8ce9946";
     };
 
+    # Tmux home dir config file
     file.".tmux.conf".text = ''
       set -g escape-time 50
 
@@ -17,11 +19,11 @@
       set -g @plugin 'nhdaly/tmux-better-mouse-mode'
 
       # Smart pane switching with awareness of vim splits
-      is_vim='echo "#{pane_current_command}" | grep -iqE "(^|\/)g?(view|n?vim?)(diff)?$"'
-      bind -n C-h if-shell "$is_vim" "send-keys C-h" "select-pane -L"
-      bind -n C-u if-shell "$is_vim" "send-keys C-u" "select-pane -U"
-      bind -n C-l if-shell "$is_vim" "send-keys C-l" "select-pane -R"
-      bind -n C-\\ if-shell "$is_vim" "send-keys C-\\" "select-pane -l"
+      is_hx='echo "#{pane_current_command}" | grep -iqE "(^|\/)g?(view|hx?)(diff)?$"'
+      bind -n C-h if-shell "$is_hx" "send-keys C-h" "select-pane -L"
+      bind -n C-u if-shell "$is_hx" "send-keys C-u" "select-pane -U"
+      bind -n C-l if-shell "$is_hx" "send-keys C-l" "select-pane -R"
+      bind -n C-\\ if-shell "$is_hx" "send-keys C-\\" "select-pane -l"
 
       # Window Splitting
       unbind %
@@ -30,12 +32,14 @@
       bind _ split-window -v -f -c '#{pane_current_path}'
       bind - split-window -v -c '#{pane_current_path}'
 
-      set -g default-terminal "xterm-256color"
+      # set -g default-terminal "xterm-256color"
+      set -g default-terminal "xterm-ghostty"
       set-option -sa terminal-overrides ",xterm*:Tc"
       set-option -g mouse on
 
       set -g status-keys vi
 
+      # Theming
       set -g @tmux-gruvbox 'dark256'
       set -g @tmux-gruvbox-statusbar-alpha 'true'
 
@@ -51,8 +55,17 @@
       unbind T
       bind T command-prompt -p "(rename-pane)" -I "#T" "select-pane -T '%%'"
 
+      # Journal
+      unbind e
       bind e display-popup -E "tmux new-session -A -s 'Journal' 'hx $HOME/workspace/journal/$(date "+%Y-%m-%d").md'"
+      # Simple shell popup
+      unbind P
+      bind P display-popup -E "tmux new-session -A -s 'Shell' '$SHELL'"
+      # Broot popup
+      unbind f
       bind f display-popup -y 45 -h 90% -E "tmux new-session -A -s 'broot' 'broot --no-tree'"
+      # Broot new window
+      unbind F
       bind F new-window -n "broot" -c "#{pane_current_path}" "broot"
 
       run '~/.tmux/plugins/tpm/tpm'
