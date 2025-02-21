9825ccc0cd06ba4f6b06020bc04f34ae0163f3e9
jacbart <jacbart@gmail.com>
2025-02-21 12:32:38 -0700

    updating how journal script works with an empty entry

--- scripts/journal/journal.zsh
+++ scripts/journal/journal.zsh
@@ -1,34 +1,37 @@
 #!/usr/bin/env zsh
 
-set +e  # Disable errexit
-set +u  # Disable nounset
-set +o pipefail  # Disable pipefail
+set +e          # Disable errexit
+set +u          # Disable nounset
+set +o pipefail # Disable pipefail
 
 function ensure_env() {
-    if [ -z "${EDITOR}" ]; then
-        echo "EDITOR is not set"
-        exit 1
-    fi
+  if [ -z "${EDITOR}" ]; then
+    echo "EDITOR is not set"
+    exit 1
+  fi
 }
 
 function open_page() {
-    page=$1
-    if [ ! -d "${HOME}/workspace/journal" ]; then
-        mkdir -p "${HOME}/workspace/journal"
-    fi
-    $EDITOR "$HOME"/workspace/journal/"$page".md
+  page=$1
+  if [ ! -d "${HOME}/workspace/journal" ]; then
+    mkdir -p "${HOME}/workspace/journal"
+  fi
+  if [ ! -f "${HOME}/workspace/journal/${page}.md" ]; then
+    echo "# ${page}" >"${HOME}/workspace/journal/${page}.md"
+  fi
+  $EDITOR "$HOME"/workspace/journal/"$page".md
 }
 
 function main() {
-    ensure_env
-    if [ $# -gt 1 ]; then
-        echo "only accepts <= 1 args: 'YYYY-MM-DD'"
-    elif [ $# -lt 1 ]; then
-        today=$(date "+%Y-%m-%d")
-        open_page "$today"
-    else
-        open_page "$1"
-    fi
+  ensure_env
+  if [ $# -gt 1 ]; then
+    echo "only accepts <= 1 args: 'YYYY-MM-DD'"
+  elif [ $# -lt 1 ]; then
+    today=$(date "+%Y-%m-%d")
+    open_page "$today"
+  else
+    open_page "$1"
+  fi
 }
 
 main "$@"
