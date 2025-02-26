ee1cdaee704fa6d602170a66c9a866d84645a9c6
jacbart <jacbart@gmail.com>
2025-02-25 21:39:18 -0700

    adding freetube and pipetube

--- home-manager/desktop/cosmic.nix
+++ home-manager/desktop/cosmic.nix
@@ -15,6 +15,8 @@
       element-desktop
       # libreoffice-qt6-fresh # office document viewer
       geeqie # image veiwer
+      unstable.freetube # youtube
+      unstable.pipeline # peertube - peer to peer video
       wl-clipboard # wl-copy and wl-paste
       vlc # multi-media viewer
     ];
