e6d68d7b548de3649c224bf658d175470cc3d081
jacbart <jacbart@gmail.com>
2025-03-03 10:18:46 -0700

    vim mode for zsh

--- home-manager/shell/zsh.nix
+++ home-manager/shell/zsh.nix
@@ -56,6 +56,9 @@ in
       enable = true;
       plugins = [
         {
+          name = "jeffreytse/zsh-vi-mode";
+        }
+        {
           name = "plugins/fzf";
           tags = [ "from:oh-my-zsh" ];
         }
