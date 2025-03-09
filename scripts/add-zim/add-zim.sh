66e22ce1f5b6b4da9e2e5768d3405b83bad558f3
jacbart <jacbart@gmail.com>
2025-03-09 16:51:53 -0600

    update add zim to better match decep

--- scripts/add-zim/add-zim.sh
+++ scripts/add-zim/add-zim.sh
@@ -8,10 +8,11 @@ ZIM_FILE=$(xh --follow "https://download.kiwix.org/zim/${PATH}" | htmlq --attrib
 if [[ $ZIM_FILE == '' ]]; then exit 1; fi
 gum confirm "Download - $PATH$ZIM_FILE?" || exit 1
 
-sudo true
+sudo true || exit 1
 if [ ! -d "${KIWIX_DIR}${PATH}" ]; then
   sudo runuser -u "${KIWIX_USER}" -- mkdir -p "${KIWIX_DIR}${PATH}"
 fi
-sudo runuser -u "${KIWIX_USER}" -- xh --follow --download "https://download.kiwix.org/zim/${PATH}${ZIM_FILE}" --output "${KIWIX_DIR}${PATH}${ZIM_FILE}"
+sudo xh --follow --download "https://download.kiwix.org/zim/${DL_FOLDER}${ZIM_FILE}" --output "${KIWIX_DIR}${DL_FOLDER}${ZIM_FILE}" || exit 1
+sudo chown -R "${KIWIX_USER}" "${KIWIX_DIR}${DL_FOLDER}${ZIM_FILE}" || exit 1
 
-sudo runuser -u "${KIWIX_USER}" -- kiwix-manage "${KIWIX_DIR}library.xml" add "${KIWIX_DIR}${PATH}${ZIM_FILE}"
+sudo runuser -u "${KIWIX_USER}" -- kiwix-manage "${KIWIX_DIR}library.xml" add "${KIWIX_DIR}${DL_FOLDER}${ZIM_FILE}" || exit 1
