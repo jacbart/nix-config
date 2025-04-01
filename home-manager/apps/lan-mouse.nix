b63c2d85465d03836833154bfb1785333f1c5527
jacbart <jacbart@gmail.com>
2025-03-31 17:23:16 -0700

    disable systemd user service for lan mouse

--- home-manager/apps/lan-mouse.nix
+++ home-manager/apps/lan-mouse.nix
@@ -7,7 +7,7 @@
 
   programs.lan-mouse = {
     enable = true;
-    systemd = true;
+    systemd = false;
     package = inputs.lan-mouse.packages.${pkgs.stdenv.hostPlatform.system}.default;
     # Optional configuration in nix syntax, see config.toml for available options
     settings = {
