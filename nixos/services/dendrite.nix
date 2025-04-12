113c8ede9c9aa17d1b7025fd000ee2c031d6a3db
jacbart <jacbart@gmail.com>
2025-04-11 17:21:41 -0700

    added in settings i found from knazarov

--- nixos/services/dendrite.nix
+++ nixos/services/dendrite.nix
@@ -1,15 +1,21 @@
 { config, pkgs, ... }: {
-  sops.secrets."matrix/private_key" = { };
+  users.groups.matrix = { };
+  sops.secrets."matrix/private_key" = {
+    mode = "0440";
+    group = config.users.groups.matrix.name;
+  };
   environment.systemPackages = [ pkgs.dendrite ];
   services.dendrite = {
     enable = true;
     httpPort = 8008;
-    # httpsPort = 8438;
     settings = {
       global = {
         server_name = "meep.sh";
         private_key = config.sops.secrets."matrix/private_key".path;
       };
     };
+  };
+  systemd.services.dendrite = {
+    serviceConfig.SupplementaryGroups = [ config.users.groups.matrix.name ];
   };
 }
