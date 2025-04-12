34928765299ea7b8ec776729c76a5eae8f6122f6
jacbart <jacbart@gmail.com>
2025-04-11 17:33:36 -0700

    dendrite registration shared secret

--- nixos/services/dendrite.nix
+++ nixos/services/dendrite.nix
@@ -4,15 +4,21 @@
     mode = "0440";
     group = config.users.groups.matrix.name;
   };
+  sops.secrets."matrix/env_file" = {
+    mode = "0440";
+    group = config.users.groups.matrix.name;
+  };
   environment.systemPackages = [ pkgs.dendrite ];
   services.dendrite = {
     enable = true;
     httpPort = 8008;
+    environmentFile = config.sops.secrets."matrix/env_file".path;
     settings = {
       global = {
         server_name = "meep.sh";
         private_key = config.sops.secrets."matrix/private_key".path;
       };
+      client_api.registration_disabled = true;
     };
   };
   systemd.services.dendrite = {
