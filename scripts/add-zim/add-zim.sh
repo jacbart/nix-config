1bed083a48d6f6a0fcbf4019cfe25555bfa2422c
jacbart <jacbart@gmail.com>
2025-08-16 13:19:21 -0700

    fix add-zim script

--- scripts/add-zim/add-zim.sh
+++ scripts/add-zim/add-zim.sh
@@ -2,15 +2,15 @@
 
 KIWIX_USER="kiwix"
 KIWIX_DIR="/var/lib/kiwix/"
-PATH=$(xh --follow "https://download.kiwix.org/zim" | htmlq --attribute href a | tail -n +7 | gum choose)
-ZIM_FILE=$(xh --follow "https://download.kiwix.org/zim/${PATH}" | htmlq --attribute href a | tail -n +6 | fzf)
+PATH_URL=$(xh --follow "https://download.kiwix.org/zim" | htmlq --attribute href a | tail -n +7 | gum choose)
+ZIM_FILE=$(xh --follow "https://download.kiwix.org/zim/${PATH_URL}" | htmlq --attribute href a | tail -n +6 | fzf)
 
 if [[ $ZIM_FILE == '' ]]; then exit 1; fi
-gum confirm "Download - $PATH$ZIM_FILE?" || exit 1
+gum confirm "Download - $PATH_URL$ZIM_FILE?" || exit 1
 
 sudo true || exit 1
-if [ ! -d "${KIWIX_DIR}${PATH}" ]; then
-  sudo runuser -u "${KIWIX_USER}" -- mkdir -p "${KIWIX_DIR}${PATH}"
+if [ ! -d "${KIWIX_DIR}${PATH_URL}" ]; then
+  sudo runuser -u "${KIWIX_USER}" -- mkdir -p "${KIWIX_DIR}${PATH_URL}"
 fi
 sudo xh --follow --download "https://download.kiwix.org/zim/${DL_FOLDER}${ZIM_FILE}" --output "${KIWIX_DIR}${DL_FOLDER}${ZIM_FILE}" || exit 1
 sudo chown -R "${KIWIX_USER}" "${KIWIX_DIR}${DL_FOLDER}${ZIM_FILE}" || exit 1
