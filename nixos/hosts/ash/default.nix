ef042aa6034c69fc754114a3bbaad41365d7a408
jacbart <jacbart@gmail.com>
2025-08-28 09:32:01 -0700

    adding steam to ash

--- nixos/hosts/ash/default.nix
+++ nixos/hosts/ash/default.nix
@@ -19,6 +19,13 @@
     # })
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
