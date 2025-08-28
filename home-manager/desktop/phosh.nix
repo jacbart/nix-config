a35845831b946d7d606a7824dc3a7d9ec717713c
jacbart <jacbart@gmail.com>
2025-08-27 22:06:38 -0700

    add diff apps for phosh

--- home-manager/desktop/phosh.nix
+++ home-manager/desktop/phosh.nix
@@ -1,8 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  imports = [
-    ../apps/firefox.nix
-  ];
+  # imports = [
+  #   ../apps/firefox.nix
+  # ];
+  home = {
+    packages = with pkgs; [
+      unstable.bitwarden-desktop
+      unstable.element-desktop
+      unstable.librewolf
+      unstable.vivaldi
+    ];
+  };
   programs.gpg.enable = true;
 
   services.gpg-agent.enable = true;
