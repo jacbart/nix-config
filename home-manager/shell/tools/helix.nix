6a852d2b57c902b56b6730f1215a7ecfca988b6c
jacbart <jacbart@gmail.com>
2025-09-14 07:47:58 -0700

    remove the option enableSteel in favor of just enabling it for helix

--- home-manager/shell/tools/helix.nix
+++ home-manager/shell/tools/helix.nix
@@ -10,7 +10,7 @@ let
   #     ./patches/helix/clickable-buffer.patch
   #   ];
   # });
-  helix = inputs.helix.packages.${pkgs.system}.helix.override { enableSteel = true; };
+  helix = inputs.helix.packages.${pkgs.system}.helix;
 in
 {
   # Home settings
--- home-manager/shell/tools/helix/init.scm
+++ home-manager/shell/tools/helix/init.scm
@@ -1,19 +1,6 @@
 (require "mattwparas-helix-package/splash.scm")
-(require "steel-pty/term.scm")
 
 (when (equal? (command-line) '("hx"))
   (show-splash))
 
 (require (prefix-in navigator. "hx-tmux-navigator/navigator.scm"))
-
-(provide open-term
-         new-term
-         kill-active-terminal
-         switch-term
-         term-resize
-         (contract/out set-default-terminal-cols! (->/c int? void?))
-         (contract/out set-default-terminal-rows! (->/c int? void?))
-         (contract/out set-default-shell! (->/c string? void?))
-         open-debug-window
-         close-debug-window
-         hide-terminal)
