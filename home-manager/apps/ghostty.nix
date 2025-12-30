09faa5e27bdb813cc67478bfc8bf54c1862b7383
jacbart <jacbart@gmail.com>
2025-12-30 06:46:14 -0700

    ghostty changes

--- home-manager/apps/ghostty.nix
+++ home-manager/apps/ghostty.nix
@@ -3,11 +3,12 @@
   programs.ghostty = {
     enable = true;
     package = pkgs.ghostty;
-    enableBashIntegration = true;
     enableZshIntegration = true;
     settings = {
       theme = "dark:Gruvbox Dark,light:Gruvbox Light";
       shell-integration = "zsh";
+      window-decoration = "none";
+      maximize = "true";
     };
   };
 }
