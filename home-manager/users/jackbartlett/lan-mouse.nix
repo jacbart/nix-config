173e7b737a226cafdb0b762cb4170613d722601c
jacbart <jacbart@gmail.com>
2026-01-24 21:48:58 -0700

    switching lanmouse target config

--- home-manager/users/jackbartlett/lan-mouse.nix
+++ home-manager/users/jackbartlett/lan-mouse.nix
@@ -24,18 +24,18 @@
       clients = [
         {
           position = "right";
-          hostname = "boojum";
+          hostname = "cork";
           activate_on_startup = false;
           ips = [
-            "192.168.0.253"
-            "100.118.9.78"
+            "192.168.0.137"
+            "100.113.192.12"
           ];
           port = 4242;
         }
       ];
       authorized_fingerprints = {
-        "9f:30:6c:ed:88:5e:06:f0:86:32:e1:12:01:5b:1e:31:8f:79:e2:94:25:57:94:8e:18:5e:8d:73:e3:c8:79:b8" =
-          "boojum-inbound";
+        "f1:bd:d1:77:33:22:08:6a:1a:b9:d5:6a:fc:c5:78:c2:f9:34:99:50:95:65:c8:8c:c3:92:c3:6d:57:13:16:18" =
+          "cork-inbound";
       };
     };
   };
