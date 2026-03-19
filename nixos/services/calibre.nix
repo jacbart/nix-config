6055a1fe83a172f9e3699f6abed74e775c649d97
jacbart <jacbart@gmail.com>
2026-03-19 12:48:51 -0700

    update calibre nginx settings

--- nixos/services/calibre.nix
+++ nixos/services/calibre.nix
@@ -20,18 +20,17 @@ in
 
   services.nginx = {
     enable = true;
+    clientMaxBodySize = "100m";
     virtualHosts."${subdomain}.${domain}" = {
       addSSL = true;
       useACMEHost = domain;
       locations."/" = {
         proxyPass = "http://127.0.0.2:8235";
-        proxyWebsockets = true; # needed if you need to use WebSocket
-        extraConfig =
-          # required when the target is also TLS server with multiple hosts
-          "proxy_ssl_server_name on;"
-          +
-            # required when the server wants to use HTTP Authentication
-            "proxy_pass_header Authorization;";
+        extraConfig = ''
+          # Disable buffering for smooth downloads
+          proxy_buffering off;
+          proxy_read_timeout 300s;
+        '';
       };
     };
   };
