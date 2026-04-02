1fc3a43ab53ad0e7b94e4e39414e52a602954e86
jacbart <jacbart@gmail.com>
2026-04-01 18:39:36 -0700

    add nixos cache key

--- modules/home/core.nix
+++ modules/home/core.nix
@@ -61,6 +61,7 @@
             trusted-public-keys = [
               "nix-cache.${vars.domain}-1:q58+Lt6h68AmBke4wpJatSrpe1cZvDzVNDTp8qurEbs="
               "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             ];
           };
         }
