8bd0e7b31f4cdbbe2c88dfdce9db2fefcf6a4264
jacbart <jacbart@gmail.com>
2026-09-06 13:28:57 -0700

    update brew and lock

--- modules/home/users/jackbartlett/default.nix
+++ modules/home/users/jackbartlett/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   vars,
   ...
 }:
@@ -8,6 +9,13 @@
     # ../../apps/wezterm.nix
     # ../../apps/zed-editor.nix
   ];
+
+  # newsboat's freshrss-passwordfile. The FreshRSS API password is the same
+  # as the login password, which nix-secrets already holds.
+  sops.secrets."freshrss/admin-password" = {
+    path = "${config.home.homeDirectory}/.config/newsboat/freshrss-password";
+    mode = "0400";
+  };
 
   # home.packages = with pkgs; [
   #   unstable.gitu
